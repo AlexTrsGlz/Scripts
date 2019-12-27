7421bbd62bad805d907e3f4f1d137f4508ba230f
Alejandro <58958639+AlexTrsGlz@users.noreply.github.com>
2019-12-27 13:26:39 -0600

    Create StoreProcedureEmpty.sql

---
+++ SQL/StoreProcedureEmpty.sql
@@ -0,0 +1,16 @@
+-- =============================================
+-- Script Template
+-- =============================================
+-- =============================================
+-- Author:		  <>
+-- Create date: <>
+-- Description:	<>
+-- =============================================
+CREATE PROCEDURE [dbo].[CMS_InsertEventLog]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+END
