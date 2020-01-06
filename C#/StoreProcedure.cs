f3820fa30f95e8c889f37ea8afcef917521cd5a7
Alejandro <58958639+AlexTrsGlz@users.noreply.github.com>
2020-01-06 13:51:02 -0600

    Update StoreProcedure.cs
    Actualizacion correcta de como utilizar un procedimiento almacenado

--- C#/StoreProcedure.cs
+++ C#/StoreProcedure.cs
@@ -1,10 +1,28 @@
-public static DataSet GetStoredProcedure(string ConnectionString, int idParam)
+public int GetStoredProcedure(string ConnectionString,string NombreProcedimiento ,int? idParam)
 {
-    ////Obtener arreglo con valores
-    SqlParameter[] arrParams = new SqlParameter[1];
-    arrParams[0] = new SqlParameter("@idParam", idParam);
+	////Obtener arreglo con valores
+	SqlParameter[] arrParams = new SqlParameter[1];
+	arrParams[0] = new SqlParameter("@idParam", SqlDbType.Int);
 
-    ////Asignación de valores
-    arrParams[0].Value = (idParam > 0)? idParam:null;
-    return SqlHelper.ExecuteDataset(ConnectionString, CommandType.StoredProcedure, "NombreProcedimientoAlmacenado", arrParams);
+	////Asignación de valores
+	arrParams[0].Value = (idParam > 0) ? idParam : null;
+
+	int idEventLog = -1;
+	//Ejecucion de procedimiento almacenado
+	using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["CMSConnectionString"].ConnectionString))
+	{
+		using (SqlCommand command = new SqlCommand(NombreProcedimiento, connection))
+		{
+			command.CommandType = CommandType.StoredProcedure;
+			command.CommandTimeout = connection.ConnectionTimeout;
+
+			command.Parameters.AddRange(arrParams);
+			connection.Open();
+			objReturned = command.ExecuteScalar();
+			connection.Close();
+		}
+	}
+
+	int.TryParse(objReturned.ToString(), out idEventLog);
+	return idEventLog;
 }
